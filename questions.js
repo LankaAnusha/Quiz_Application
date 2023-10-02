5639ba5b056708169127fad6eb08859ed5af510e
LankaAnusha <105148909+LankaAnusha@users.noreply.github.com>
2023-10-02 17:46:43 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What are the important tags in HTML?",
+    answer: "html, head, and body",
+    options: [
+      "title, head, and meta",
+      "html, head, and body",
+      "link, body, and script",
+      "All the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does Hoisting mean in Javascript?",
+    answer: "It is a special Javascript behaviour",
+    options: [
+      "It is a data type",
+      "It is a special Javascript behaviour",
+      "It is an object",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is Ecmascript",
+    answer: "New Version of Javascript",
+    options: [
+      "Type of HTML",
+      "Type of CSS",
+      "New Version of Javascript",
+      "Both 1 and 2"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Why is CSS used in FrontEnd?",
+    answer: "Styling the webpage",
+    options: [
+      "Styling the webpage",
+      "create structure to the webpage",
+      "providing behaviour to the webpage",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
